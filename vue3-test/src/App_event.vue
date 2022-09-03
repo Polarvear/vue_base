09152688b5bc57ab425ab004abb64b88f34dd3c7
Polarvear <znzpzp13@naver.com>
2022-09-03 23:37:42 +0900

    vue lifecycle/evnet memo update

---
+++ vue3-test/src/App_event.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1 v-bind:[attr]="'active'"  <= 이렇게하면 하나의 문자데이터로 적용할 수 있음 class이름에 느낌표 계속 붙는 상황 수정
+    @[event]='add'>
+    {{msg}}
+  </h1>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      msg: 'active',
+      attr: 'class',
+      event: 'click' //@click 말고 이렇게도 줄 수 있음
+    }
+  },
+  methods: { // methods로 써줘야함
+    add() {
+      this.msg += "!"
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .active {
+    color: aqua;
+  }
+</style>
---
+++ vue3-test/src/App_lifecycle.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2
+    }
+  },
+  beforeCreate() { // 활용도 낮음
+    console.log('333333', this.count); // 얘는 데이터가 정의되기전에 실행된다.이러면 count가 undefined
+  },
+  created() {
+    console.log('222222!', this.count); // 순서에 상관 없음~~
+  },
+  beforeMount() {
+    console.log('순서에 상관없다~~'); // 그래도 가독성을 위해서 순서 맞춰줘야함
+    console.log(document.querySelector('h1')); // 여기서는 null 발생 why? 구조가 생성되기 전이기 때문에
+  },
+  mounted() {
+    console.log('111111');
+    console.log(document.querySelector('h1')); // 여기서는 잘나온다.
+  },
+  beforeCreate() { // 활용도 낮음
+    console.log('4324343', this.count); // 얘는 데이터가 정의되기전에 실행된다.이러면 count가 undefined, 순서 상관없다~
+  },
+}
+</script>
