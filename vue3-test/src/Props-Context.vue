ac20e6d767b05a09c3d452e3c9985e59a814f7b1
Polarvear <znzpzp13@naver.com>
2022-09-07 02:23:11 +0900

    props_context & this개념

---
+++ vue3-test/src/Props-Context.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    v-bind="$attrs"
+    class="btn"
+    @click="hello">
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+import { onMounted } from 'vue'
+export default {
+  inheritAttrs: false,
+  props: {
+    color: {
+      type: String,
+      default: 'gray'
+    }
+  },
+  emits: ['hello'],
+  // mounted() {
+  //   console.log(this.color);
+  //   console.log(this.$attrs);
+  // },
+  // methods: {
+  //   hello() {
+  //     this.$emit('hello')
+  //   }
+  // },
+  setup(props, context) { //this 를 가지고 올 수 없기 떄문에 매개변수로 넘겨줌
+    function hello() {
+      context.emit('hello') // $ 표시 없애야함
+    }
+     onMounted(() => {
+      console.log(props.color);
+      console.log(context.attrs);
+    })
+
+    return {
+      hello
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
