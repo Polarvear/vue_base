bc703095c1d765b537726ffcbc80858701fee6a7
Polarvear <znzpzp13@naver.com>
2022-09-07 02:01:41 +0900

    composition API base

---
+++ vue3-test/src/Composition_base.vue
@@ -0,0 +1,28 @@
+<template>
+  <div @click="increase">
+    {{ count }}
+  </div>
+</template>
+
+
+<script>
+import { ref } from 'vue'
+export default {
+
+  setup() {
+    let count = ref(0) // 이런식으로 반응성 가지게 할 수 있음
+      function increase() {
+        count.value += 1 // .value를 통해서 접근가능
+      }
+
+    return {
+      count,
+      increase
+    }  
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
